9e9abbead027f62037fcd8747862a19e04966edc
milena-m989 <milenasrb29@hotmail.com>
2022-01-12 18:31:44 +0100

    Added flags png.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,21 @@
 $(function () {
     $(document).scroll(function () {
-        var $header = $("#header");
-        var $hero = $(".img-nis");
+        var $header = $('#header');
+        var $hero = $('.img-nis');
         $header.toggleClass('scrolled', $(this).scrollTop() > $hero.height() - $header.height());
     });
+    $('#second-lang').click(function (){
+        var $firstLang = $('#first-lang');
+        var $secondLang = $('#second-lang');
+        var $firstLangClass = $firstLang.attr('class');
+        var $firstLangText = $firstLang.text();
+        var $secondLangClass = $secondLang.attr('class');
+        var $secondLangText = $secondLang.text();
+        $firstLang.removeClass($firstLangClass);
+        $firstLang.addClass($secondLangClass);
+        $secondLang.removeClass($secondLangClass);
+        $secondLang.addClass($firstLangClass);
+        $firstLang.text($secondLangText);
+        $secondLang.text($firstLangText);
+    }) 
 });
